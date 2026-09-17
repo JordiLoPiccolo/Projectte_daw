55ce8dac89e808ff108b1fa9015e1652da898c3c
jordi <jordicos16@gmail.com>
2026-09-17 18:50:33 +0200

    spotify login remastered

--- spotify_login.ts
+++ spotify_login.ts
@@ -1,11 +1,49 @@
+// la versio simple
+
+interface usuari {
+    correu: string,
+    contrasenya: string,
+    edat: number;
+}
+
+function autentificacio(usuari: usuari): boolean {
+
+    let correcte: boolean = false;
+    if (usuari.correu === "correuCorrecte" && usuari.contrasenya === "contrasenya" && usuari.edat > 14) {
+        correcte = true;
+    }
+
+    return correcte;
+}
+
+function imprimir(auth: boolean): void {
+    if (auth) {
+        console.log("Usuari autentificat")
+    } else {
+        console.log("Usuari, contrasenya o edat no valids")
+    }
+}
+
+const usuariCorrecte: usuari = {
+    correu: "correuCorrecte",
+    contrasenya: "contrasenya",
+    edat: 15
+}
+
+let autOk: boolean = autentificacio(usuariCorrecte)
+
+imprimir(autOk);
+console.log(autentificacio(usuariCorrecte));
+
+//la versio complicada
 function autenticar(): void {
     let correu: string = prompt("Introdueix el teu correu:") || "";
     let contrasenya: string = prompt("Introdueix la teva contrasenya:") || "";
     let edat: number = Number(prompt("Introdueix la teva edat:"));
 
-    const correuCorrecte: string = "usuari@gmail.com";
-    const contrasenyaCorrecta: string = "1234";
-    const edatMinima: number = 18;
+    const correuCorrecte: string = "ola@gmail.com";
+    const contrasenyaCorrecta: string = "6769";
+    const edatMinima: number = 14;
 
     if (
         correu === correuCorrecte &&
