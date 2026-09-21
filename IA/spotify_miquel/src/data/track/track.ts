fa6b5b4bf4b8396abb2063bd1abbc7f532a1d4aa
jordi <jordicos16@gmail.com>
2026-09-21 18:51:01 +0200

    spotify index

---
+++ IA/spotify_miquel/src/data/track/track.ts
@@ -0,0 +1,21 @@
+import { Track } from "../../interfaces/track/track";
+
+export const tracks: Track[] = [{
+    id: "3456-abd1-c2d1-ef12",
+    title: "Rattle and Hum",
+    artist: "U2",
+    duration: {
+        minutes: 2,
+        seconds: 45,
+    },
+},
+{
+    id: "3499-cdd1-cd92-ef23",
+    title: "Thriller",
+    artist: "U2",
+    duration: {
+        minutes: 3,
+        seconds: 11
+    }
+}
+];
---
+++ IA/spotify_miquel/src/index.ts
@@ -0,0 +1,38 @@
+import express, { Express, Request, Response } from "express";
+import { APICONFIG } from "./config/apiConfig";
+import { tracks } from "./data/track/track";
+import { Track } from "./interfaces/track/track";
+
+const app: Express = express();
+
+app.get("/", (_req: Request, res: Response) => { // _req → petició rebuda però no utilitzada
+    return res.json(JSON.stringify(APICONFIG));
+});
+
+
+app.get("/tracks", (_req: Request, res: Response) => { // _req → petició rebuda però no utilitzada
+    return res.status(200).json(tracks);
+});
+
+
+/*app.get("/tracks/news") {
+
+
+}*/
+
+app.get("/tracks/:id", (req: Request, res: Response) => { // _req → petició rebuda però no utilitzada
+    const idTrack: string = req.params.id as string;
+    const track: Track[] = tracks.filter(
+        (t: Track) => { return t.id === idTrack }
+    );
+
+    if (track.length === 0) {
+        return res.status(404).json({ message: `Track ${idTrack} not found lol` });
+    }
+    return res.status(200).json(track);
+});
+
+
+app.listen(APICONFIG.port, APICONFIG.host, () => {
+    console.log(`Servidor escoltant a ${APICONFIG.host}:${APICONFIG.port}`);
+});
