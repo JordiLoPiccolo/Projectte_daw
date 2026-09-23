16806d12edd506a69bf6d915b0252b2bd7b9f8e9
jordi <jordicos16@gmail.com>
2026-09-23 20:20:06 +0200

    importar cancons a app

---
+++ Client/app/sputifay/src/data/canco.ts
@@ -0,0 +1,30 @@
+import type { Canco } from "../interface/canco";
+
+export const cancons: Canco[] = [
+
+    {
+        id: "2B-CA",
+        titol: "Rattle and Ham",
+        artista: "U2",
+        durada: 90
+    },
+    {
+        id: "oj-ek",
+        titol: "Fist of fury",
+        artista: "U2",
+        durada: 190
+    },
+    {
+        id: "5f-JK",
+        titol: "Chicago",
+        artista: "Michael Jackson",
+        durada: 190
+    },
+    {
+        id: "Df-ei",
+        titol: "Thriller",
+        artista: "Michael Jackson",
+        durada: 180
+
+    }
+]
